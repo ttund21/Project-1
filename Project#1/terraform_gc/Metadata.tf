8f935e01d8feaa92056f6be4961f930035cd9a68
Damocles <40272778+ttund21@users.noreply.github.com>
2019-05-24 17:11:51 -0300

    Update Metadata.tf

--- Project#1/terraform_gc/Metadata.tf
+++ Project#1/terraform_gc/Metadata.tf
@@ -1,4 +1,4 @@
-resource "google_compute_project_metadata_item" "Damocles" {
+resource "google_compute_project_metadata_item" "user" {
  key   = "ssh-keys"
  value = "User:${file("~/.ssh/id_rsa.pub")}" 
 }
