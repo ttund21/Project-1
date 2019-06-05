f3d9bc756cd07f9adfd8b3a38d2018de05755598
Damocles <40272778+ttund21@users.noreply.github.com>
2019-06-05 08:25:46 -0300

    Update ec2.tf

--- Project#1/terraform_aws/ec2.tf
+++ Project#1/terraform_aws/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami                         = "ami-09f4cd7c0b533b081"
   instance_type               = "t2.micro"
-  key_name                    = "Terraform"
+  key_name                    = "${aws_key_pair.terraform.id}"
   vpc_security_group_ids      = ["${aws_security_group.allow_ssh_http.id}"]
   subnet_id                   = "subnet-52df0f09"
 }
